1f57eabd2e6fddff470a825895c26952025849b6
waterm310n <wat3rmelon2000@gmail.com>
2024-05-30 15:08:41 +0800

    step 3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -5,6 +5,23 @@
 #[cfg(feature = "axstd")]
 use axstd::println;
 
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
 const PLASH_START: usize = 0x22000000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
@@ -29,7 +46,30 @@ fn main() {
     println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
 
     println!("Load payload ok!");
+
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+
     println!("Execute app ...");
+    let arg0: u8 = b'A';
+
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
+    )}
 
     // execute app
     unsafe { core::arch::asm!("
